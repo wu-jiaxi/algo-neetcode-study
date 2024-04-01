a857cb03499351bc327d374ae03696d94471e5ed
wu-jiaxi <dev.jiaxiwu@gmail.com>
2024-03-31 23:43:25 -0400

    readded algo

--- algo.js
+++ algo.js
@@ -1,4 +1,34 @@
-function determineWinner(votes) {}
+function determineWinner(votes) {
+  let voteCount = {};
+
+  votes.forEach((vote) => {
+    if (voteCount[vote]) {
+      voteCount[vote]++;
+    } else {
+      voteCount[vote] = 1;
+    }
+  });
+
+  let voteCountsArray = Object.entries(voteCount).map(([candidate, count]) => ({
+    candidate,
+    count,
+  }));
+
+  voteCountsArray.sort((a, b) => b.count - a.count);
+
+  const winners = [];
+  let highestVoteCount = 0;
+
+  for (const candidate of voteCountsArray) {
+    if (candidate.count >= highestVoteCount) {
+      highestVoteCount = candidate.count;
+      winners.push(candidate.candidate);
+    } else {
+      break;
+    }
+  }
+  return winners;
+}
 
 const votes = ["A", "B", "C", "D", "A", "B", "C", "A", "D"];
 const winners = determineWinner(votes);
