2bd1099f337c1754db4e8ce3f81383f4e6af6449
wu-jiaxi <dev.jiaxiwu@gmail.com>
2024-03-31 23:00:19 -0400

    count array

--- algo.js
+++ algo.js
@@ -1,34 +1,4 @@
-determineWinner = (votes) => {
-  let voteCounts = {};
-
-  votes.forEach((vote) => {
-    if (voteCounts[vote]) {
-      voteCounts[vote]++;
-    } else {
-      voteCounts[vote] = 1;
-    }
-  });
-
-  const votesCountArray = Object.entries().map(([candidate, count]) => ({
-    candidate,
-    count,
-  }));
-
-  votesCountArray.sort((a, b) => b.count - a.count);
-
-  const winners = [];
-  let highestVoteCount = 0;
-
-  for (const candidate of votesCountArray) {
-    if (candidate.count >= highestVoteCount) {
-      highestVoteCount = candidate.count;
-      winners.push(candidate.candidate);
-    } else {
-      break;
-    }
-  }
-  return winners;
-};
+function determineWinner(votes) {}
 
 const votes = ["A", "B", "C", "D", "A", "B", "C", "A", "D"];
 const winners = determineWinner(votes);
