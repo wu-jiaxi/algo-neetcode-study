b5f8d4bda6a10cd2de4331a3e26197db7ae13374
wu-jiaxi <dev.jiaxiwu@gmail.com>
2024-04-02 23:17:37 -0400

    highest count

--- algo.js
+++ algo.js
@@ -1,5 +1,32 @@
 function determineWinner(votes) {
-  
+  let voteCount = {};
+
+  votes.forEach((vote) => {
+    if (voteCount[vote]) {
+      voteCount[vote]++;
+    } else {
+      voteCount[vote] = 1;
+    }
+  });
+
+  const voteCountArray = Object.entries(voteCount).map(
+    ([candidate, count]) => ({ candidate, count })
+  );
+
+  voteCountArray.sort((a, b) => b.count - a.count);
+
+  let winners = [];
+  let highestCount = 0;
+
+  for (const candidate of voteCountArray) {
+    if (candidate.count >= highestCount) {
+      highestCount = candidate.count;
+      winners.push(candidate.candidate);
+    } else {
+      break;
+    }
+  }
+  return winners;
 }
 
 const votes = ["A", "B", "C", "D", "A", "B", "C", "A", "D"];
