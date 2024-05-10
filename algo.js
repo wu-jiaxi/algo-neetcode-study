d9833b15c319be82d1c5ee1c36cd2ed2780c617c
wu-jiaxi <dev.jiaxiwu@gmail.com>
2024-05-09 21:50:46 -0400

    1431. Kids With the Greatest Number of Candies

--- algo.js
+++ algo.js
@@ -7,3 +7,27 @@ function determineWinner(votes) {
 const votes = ["A", "B", "C", "D", "A", "B", "C", "A", "D"];
 const winners = determineWinner(votes);
 console.log("Winner(s):", winners);
+
+// 1431. Kids With the Greatest Number of Candies
+var kidsWithCandies = function (candies, extraCandies) {
+  let greatestCandy = 0;
+  let newArray = [];
+  let largestNum = 0;
+  let trueFalseArray = [];
+
+  for (let i = 0; i < candies.length; i++) {
+    let sum = candies[i] + extraCandies;
+    newArray.push(sum);
+    largestNum = Math.max(...newArray);
+
+    if (newArray[i] === largestNum) {
+      trueFalseArray.push(false);
+    } else if (newArray[i] < largestNum) {
+      trueFalseArray.push(true);
+    } else {
+      trueFalseArray.push(false);
+    }
+  }
+
+  return trueFalseArray;
+};
