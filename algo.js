2cf7822c03863b7bf295cc671d933a478ec8aba6
wu-jiaxi <dev.jiaxiwu@gmail.com>
2024-05-19 21:53:45 -0400

    algos

--- algo.js
+++ algo.js
@@ -31,3 +31,26 @@ var kidsWithCandies = function (candies, extraCandies) {
 
   return trueFalseArray;
 };
+
+var kidsWithCandies = function (candies, extraCandies) {
+  let greatestCandy = 0;
+  let newArray = [];
+  let largestNum = 0;
+  let trueFalseArray = [];
+
+  for (let i = 0; i < candies.length; i++) {
+    let sum = candies[i] + extraCandies;
+    newArray.push(sum);
+    largestNum = Math.max(...newArray);
+
+    if (newArray[i] === largestNum) {
+      trueFalseArray.push(false);
+    } else if (newArray[i] < largestNum) {
+      trueFalseArray.push(true);
+    } else {
+      trueFalseArray.push(false);
+    }
+  }
+
+  return trueFalseArray;
+};
